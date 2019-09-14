694ae00a09152e4446493015a4ce2222c0ce078e
Henry Noon <henrynoon@icloud.com>
2019-09-14 17:21:49 +0100

    Add UIStackView to SectionZeroCell

--- MonzoClone/Cells/SectionZeroCell.swift
+++ MonzoClone/Cells/SectionZeroCell.swift
@@ -12,9 +12,7 @@ class SectionZeroCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = .red
-        addSubview(infoLabel)
-        infoLabel.anchor(top: topAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor)
+        setUpStackView()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -23,7 +21,23 @@ class SectionZeroCell: UICollectionViewCell {
     
     let infoLabel: UILabel = {
         let label = UILabel()
-        label.text = "These are the three cells in section 0"
+        label.text = "Groceries"
         return label
     }()
+    
+    let usefulIcon: UIImageView = {
+        let icon = UIImageView(image: #imageLiteral(resourceName: "Sainsbury's Logo"))
+        let iconSize = CGSize(width: 30, height: 30)
+        icon.widthAnchor.constraint(equalToConstant: iconSize.width).isActive = true
+        icon.heightAnchor.constraint(equalToConstant: iconSize.height).isActive = true
+        return icon
+    }()
+    
+    fileprivate func setUpStackView() {
+        let stackView = UIStackView(arrangedSubviews: [usefulIcon, infoLabel])
+        stackView.axis = .horizontal
+        stackView.spacing = 16
+        addSubview(stackView)
+        stackView.anchor(top: topAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 10, left: 16, bottom: 10, right: 10))
+    }
 }
