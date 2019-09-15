ca6383616e0361a44e11a3784c5a194e7f2f80f6
Henry Noon <henrynoon@icloud.com>
2019-09-15 16:14:56 +0100

    Create PaymentInfo class as the model

---
+++ MonzoClone/Model/PaymentInfo.swift
@@ -0,0 +1,16 @@
+//
+//  PaymentInfo.swift
+//  MonzoClone
+//
+//  Created by Henry Noon on 15/09/2019.
+//  Copyright © 2019 Henry Noon. All rights reserved.
+//
+
+import UIKit
+
+class PaymentInfo: NSObject {
+
+    var usefulIcon: String?
+    var titleText: String?
+    
+}
