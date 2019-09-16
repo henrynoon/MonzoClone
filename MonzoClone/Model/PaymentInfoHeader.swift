ceb373466d9cf08613f6c44fd4f59ed5884eb17b
Henry Noon <henrynoon@icloud.com>
2019-09-16 10:00:14 +0100

    Create PaymentInfoHeader class as the model

---
+++ MonzoClone/Model/PaymentInfoHeader.swift
@@ -0,0 +1,20 @@
+//
+//  PaymentInfoHeader.swift
+//  MonzoClone
+//
+//  Created by Henry Noon on 15/09/2019.
+//  Copyright © 2019 Henry Noon. All rights reserved.
+//
+
+import UIKit
+
+class PaymentInfoHeader: NSObject {
+
+    var latitude: String?
+    var longitude: String?
+    var logo: String?
+    var name: String?
+    var address: String?
+    var price: String?
+
+}
