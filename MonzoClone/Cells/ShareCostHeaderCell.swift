3f7728e11131537cd1d7d419f482bf956f171cd8
Henry Noon <henrynoon@icloud.com>
2019-09-17 11:55:01 +0100

    Create sectionLabel in ShareCostHeaderCell

--- MonzoClone/Cells/ShareCostHeaderCell.swift
+++ MonzoClone/Cells/ShareCostHeaderCell.swift
@@ -12,10 +12,19 @@ class ShareCostHeaderCell: UICollectionReusableView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = .purple
+        self.backgroundColor = UIColor(displayP3Red: 0.97, green: 0.97, blue: 0.97, alpha: 1)
+        addSubview(sectionLabel)
+        sectionLabel.anchor(top: nil, leading: leadingAnchor, bottom: bottomAnchor, trailing: nil, padding: .init(top: 0, left: 16, bottom: 8, right: 0))
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    let sectionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12, weight: .light)
+        label.textColor = UIColor(displayP3Red: 0.44, green: 0.48, blue: 0.55, alpha: 1)
+        return label
+    }()
 }
