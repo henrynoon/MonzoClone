5bcc3c57280b07ea54bea0f3ddb0cab72e685b68
Henry Noon <henrynoon@icloud.com>
2019-09-17 12:32:27 +0100

    Create ReuseSectionHeader class as the model

---
+++ MonzoClone/Model/ReuseSectionHeader.swift
@@ -0,0 +1,15 @@
+//
+//  ReuseSectionHeader.swift
+//  MonzoClone
+//
+//  Created by Henry Noon on 17/09/2019.
+//  Copyright © 2019 Henry Noon. All rights reserved.
+//
+
+import UIKit
+
+class ReuseSectionHeader: NSObject {
+    
+    var title: String?
+    
+}
