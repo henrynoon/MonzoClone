d0f16fa44e2df6b9c833ce3a89fdd2279c1af9fa
Henry Noon <henrynoon@icloud.com>
2019-09-17 18:20:33 +0100

    Create Footer class as the model

---
+++ MonzoClone/Model/Footer.swift
@@ -0,0 +1,15 @@
+//
+//  Footer.swift
+//  MonzoClone
+//
+//  Created by Henry Noon on 17/09/2019.
+//  Copyright © 2019 Henry Noon. All rights reserved.
+//
+
+import UIKit
+
+class Footer: NSObject {
+    
+    var title: String?
+
+}
