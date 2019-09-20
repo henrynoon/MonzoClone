94f5ab2d66ca4d7e4417610baa8df0d6802678eb
Henry Noon <henrynoon@icloud.com>
2019-09-20 14:42:38 +0100

    Create LabelWithLabel as the model

---
+++ MonzoClone/Model/LabelWithLabel.swift
@@ -0,0 +1,17 @@
+//
+//  LabelWithLabel.swift
+//  MonzoClone
+//
+//  Created by Henry Noon on 20/09/2019.
+//  Copyright © 2019 Henry Noon. All rights reserved.
+//
+
+import UIKit
+
+class LabelWithLabel: NSObject {
+    
+    var title: String?
+    var subtitle: String?
+    var detail: String?
+
+}
