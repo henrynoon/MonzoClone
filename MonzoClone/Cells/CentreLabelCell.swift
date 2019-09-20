f7d4ad3a6e19ff2dc8460fb42e3ea42187a30282
Henry Noon <henrynoon@icloud.com>
2019-09-20 11:41:43 +0100

    Create UILabel in CentreLabelCell

--- MonzoClone/Cells/CentreLabelCell.swift
+++ MonzoClone/Cells/CentreLabelCell.swift
@@ -12,11 +12,23 @@ class CentreLabelCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = .orange
+        self.backgroundColor = .white
+        self.layer.borderColor = UIColor(red: 0.78, green: 0.78, blue: 0.8, alpha: 1).cgColor
+        self.layer.borderWidth = 0.5
+        addSubview(mainLabel)
+        mainLabel.translatesAutoresizingMaskIntoConstraints = false
+        mainLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        mainLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    let mainLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Improve name, location or logo"
+        label.font = .systemFont(ofSize: 15, weight: .regular)
+        return label
+    }()
 }
