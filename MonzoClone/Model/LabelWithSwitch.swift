191072586f65ca8d9d3124573be427d3bcf25c82
Henry Noon <henrynoon@icloud.com>
2019-09-20 10:48:47 +0100

    Create LabelWithSwitch as the model

---
+++ MonzoClone/Model/LabelWithSwitch.swift
@@ -0,0 +1,16 @@
+//
+//  LabelWithSwitch.swift
+//  MonzoClone
+//
+//  Created by Henry Noon on 20/09/2019.
+//  Copyright © 2019 Henry Noon. All rights reserved.
+//
+
+import UIKit
+
+class LabelWithSwitch: NSObject {
+    
+    var title: String?
+    var subtitle: String?
+
+}
