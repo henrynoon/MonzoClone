de37e8b621895d7840c51e252139314d33cc5c66
Henry Noon <henrynoon@icloud.com>
2019-09-20 11:51:26 +0100

    Create CentreLabel as the model

---
+++ MonzoClone/Model/CentreLabel.swift
@@ -0,0 +1,16 @@
+//
+//  CentreLabel.swift
+//  MonzoClone
+//
+//  Created by Henry Noon on 20/09/2019.
+//  Copyright © 2019 Henry Noon. All rights reserved.
+//
+
+import UIKit
+
+class CentreLabel: NSObject {
+    
+    var title: String?
+    var colour: UIColor?
+
+}
