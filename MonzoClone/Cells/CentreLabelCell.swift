55ba181692c9b7cc9e8d41c25b8e48d92a67c8d9
Henry Noon <henrynoon@icloud.com>
2019-09-22 16:40:42 +0100

    Add separator line between CentreLabelCell cells

--- MonzoClone/Cells/CentreLabelCell.swift
+++ MonzoClone/Cells/CentreLabelCell.swift
@@ -13,12 +13,8 @@ class CentreLabelCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = .white
-        self.layer.borderColor = UIColor(red: 0.78, green: 0.78, blue: 0.8, alpha: 1).cgColor
-        self.layer.borderWidth = 0.5
-        addSubview(mainLabel)
-        mainLabel.translatesAutoresizingMaskIntoConstraints = false
-        mainLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-        mainLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        setUpLabel()
+        setUpSeparatorView()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -27,10 +23,29 @@ class CentreLabelCell: UICollectionViewCell {
     
     let mainLabel: UILabel = {
         let label = UILabel()
-        label.text = "Improve name, location or logo"
         label.font = .systemFont(ofSize: 15, weight: .regular)
         return label
     }()
+    
+    let separatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 0.78, green: 0.78, blue: 0.8, alpha: 1)
+        return view
+    }()
+    
+    fileprivate func setUpLabel() {
+        addSubview(mainLabel)
+        mainLabel.translatesAutoresizingMaskIntoConstraints = false
+        mainLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        mainLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+    }
+    
+    fileprivate func setUpSeparatorView() {
+        addSubview(separatorView)
+        separatorView.translatesAutoresizingMaskIntoConstraints = false
+        separatorView.heightAnchor.constraint(equalToConstant: 0.5).isActive = true
+        separatorView.anchor(top: nil , leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 15, bottom: 0, right: 0))
+    }
     
     var centreLabel: CentreLabel? {
         didSet {
