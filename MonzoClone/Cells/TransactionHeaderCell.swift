e5dea5c2b3949fff92adccfd703ab48fa14f9edb
Henry Noon <henrynoon@icloud.com>
2019-09-24 15:59:51 +0100

    Create dateLabel in TransactionHeaderCell

--- MonzoClone/Cells/TransactionHeaderCell.swift
+++ MonzoClone/Cells/TransactionHeaderCell.swift
@@ -13,10 +13,29 @@ class TransactionHeaderCell: UICollectionReusableView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = UIColor(red: 0.96, green: 0.96, blue: 0.96, alpha: 1)
+        addSubview(dateLabel)
+        dateLabel.anchor(top: topAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: nil, padding: .init(top: 5, left: 15, bottom: 5, right: 0))
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    var dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12, weight: .regular)
+        label.textColor = UIColor(red: 0.08, green: 0.14, blue: 0.24, alpha: 1)
+        label.text = "SATURDAY, 15 APR"
+        return label
+    }()
+    
+    var transaction: Transaction? {
+        didSet {
+            if let date = transaction?.created {
+                dateLabel.text = date
+            }
+        }
+    }
 }
+
+
