dc14a859fa9a4a7f9cf41e0b3d8beb4b4a880179
Henry Noon <henrynoon@icloud.com>
2019-09-24 15:08:01 +0100

    Create Transaction class as the model

---
+++ MonzoClone/Model/Transaction.swift
@@ -0,0 +1,18 @@
+//
+//  Transaction.swift
+//  MonzoClone
+//
+//  Created by Henry Noon on 24/09/2019.
+//  Copyright © 2019 Henry Noon. All rights reserved.
+//
+
+import UIKit
+
+class Transaction: NSObject {
+
+    var logo: String?
+    var title: String?
+    var subtitle: String?
+    var price: String?
+    
+}
