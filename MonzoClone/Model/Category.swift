83352a072bd1e9967769a33cf84c84679704eaab
Henry Noon <henrynoon@icloud.com>
2020-02-24 11:54:29 +0000

    Create Category model

---
+++ MonzoClone/Model/Category.swift
@@ -0,0 +1,18 @@
+//
+//  Category.swift
+//  MonzoClone
+//
+//  Created by Henry Noon on 24/02/2020.
+//  Copyright © 2020 Henry Noon. All rights reserved.
+//
+
+import UIKit
+
+class Category: NSObject {
+
+    var categoryName: String?
+    var icon: String?
+    var totalSpent: String?
+    var spendingBudget: String?
+    var amountLeftToSpend: String?
+}
